f3b1c59bafcec107ba18c26fba3ffdb65620b0b0
Piotr Ożóg <piotr.ozog@zen.com>
2022-09-15 13:15:51 +0200

    update url

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "SeonSDK", path: "https://cdn.seon.io/sdk/ios/SeonSDK/4.0.0/SeonSDK-4.0.0-xcfw.zip"),
+        .binaryTarget(
+            name: "SeonSDK",
+            url: "https://cdn.seon.io/sdk/ios/SeonSDK/4.0.0/SeonSDK-4.0.0-xcfw.zip"),
     ]
 )
